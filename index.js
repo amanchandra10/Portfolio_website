82ef583adf8dd3de115c40daeb2e354c7e52a87d
Aman Chandra <aman123chandra@gmail.com>
2024-03-08 12:10:15 +0530

    Updatess

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ function experience(){
     con.innerHTML=
     `<h1 style="color:aliceblue;">Experience</h1>
     <ul class="ul1">
-    <li><p>1 month internship experience in The  Codeclause.</p></li>
-    <li><p>6 month internship experience in The Predulive Labs.</p></li>
-    <li><p>1 month internship experience in The Sparks Foundation.</p></li>
-    <li><p>1 month internship experience in Oasis Infobyte.</p></li>
+    <li><p>1 month internship experience in The  Codeclause. <a style="color: rgba(255, 179, 37, 0.696);" href="https://github.com/amanchandra10/CodeClauseInternship_Basic_Chat_App" target="main">Project 1</a> <a style="color: rgba(255, 179, 37, 0.696);" href="https://github.com/amanchandra10/CodeClauseInternship_News_Portal" target="main">Project 2</a> <a style="color: rgba(255, 179, 37, 0.696);" href="https://github.com/amanchandra10/CodeClauseInternship_Basic_Text_Editor" target="main">Project 3</a></p></li>
+    <li><p>6 month internship experience in The Predulive Labs. <a style="color: rgba(255, 179, 37, 0.696);" href="https://betaecom.onrender.com/" target="main">Project Link</a></p></li>
+    <li><p>1 month internship experience in The Sparks Foundation. <a style="color: rgba(255, 179, 37, 0.696);" href="https://github.com/amanchandra10/Spark_Foundation_Task1" target="main">Project Link</a></p></li>
+    <li><p>1 month internship experience in Oasis Infobyte. <a style="color: rgba(255, 179, 37, 0.696);" href="https://amanchandra10.github.io/OIBSIP-To-Do-Webapp/" target="main">Project 1</a> <a style="color: rgba(255, 179, 37, 0.696);" href="https://amanchandra10.github.io/OIBSIP-Calculator/" target="main">Project 2</a> <a style="color: rgba(255, 179, 37, 0.696);" href="https://amanchandra10.github.io/OIBSIP-Tribute-page/" target="main">Project 3</a></p></li>
     </ul>`
 }
 function education(){
@@ -48,9 +48,9 @@ function education(){
     <li><p style="color:red;"><u>2021-2025</u></p></li>
     <li><p>Currently pursuing BTECH from branch CSE from BBDNIIT LUCKNOW.</p></li>
     <li><p style="color:red;"><u>2020</u></p></li>
-    <li><p>Completed 12th from KVS Gomtinagar Lucknow(2020) with 70%.</p></li>
+    <li><p>Completed 12th from KVS Gomtinagar Lucknow(2020).</p></li>
     <li><p style="color:red;"><u>2018</u></p></li>
-    <li><p>Completed 10th from KVS Gomtinagar Lucknow(2018) with 70%.</p></li>
+    <li><p>Completed 10th from KVS Gomtinagar Lucknow(2018).</p></li>
     </ul>`
 }
 
